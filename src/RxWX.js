6f0c797cae81bcae711199a9104af1d34c5cb29e
delong zhu <yalishizhude@163.com>
2017-09-25 16:59:17 +0800

    fix bug

--- src/RxWX.js
+++ src/RxWX.js
@@ -7,27 +7,16 @@ for (let p in wx) {
       ob[p] = Object.assign(wx[p])
       break;
     case 'function':
-      if (p.includes('Sync')) {
+      if (/Sync$/.test(p)) {
         ob[p] = (obj) => Rx.Observable.of(wx[p].call(null, obj))
       } else {
-        ob[p] = (obj) => {
-          let param = Object.assign(obj || {})
-          return Rx.Observable.create(observe => {
-            param.success = (arg) => {
-              observe.next(arg)
-              observe.complete()
-            }
-            param.fail = (e) => {
-              console.error(e)
-              observe.throw(e)
-            }
-            param.complete = (res) => {
-              observe.next(res)
-              observe.complete()
-            }
-            wx[p].call(null, param)
-          })
-        }
+        ob[p] = (obj) => Rx.Observable.create(observe => {
+          let param = Object.assign({}, obj)
+          param.success = (...arg) => observe.next(...arg)
+          param.fail = (e) => observe.error(e)
+          param.complete = (res) => observe.complete()
+          wx[p].call(null, param)
+        })
       }
       break;
     default:
